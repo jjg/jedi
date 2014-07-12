7dc0542e10252695ca84e4127dd0f6b054a325fb
Jason J. Gullickson <jason.gullickson@gmail.com>
2014-07-12 02:08:52 -0500

    initial commit

---
+++ js/jedi.js
@@ -0,0 +1,35 @@
+function loadFile(filename, cb){
+
+	var fileReq = new XMLHttpRequest();
+	fileReq.open('GET', 'http://localhost:1313/' + filename, true);
+	fileReq.onreadystatechange = fileReqRes;
+	fileReq.send();
+
+	function fileReqRes(){
+		if(this.readyState == 4){
+			if(this.status == 200){
+				cb(this.responseText);
+			} else {
+				cb(null);
+			}	
+		}
+	}	
+}
+
+function saveFile(filename, contents, cb){
+
+	var saveFileReq = new XMLHttpRequest();
+	saveFileReq.open('POST', 'http://localhost:1313/' + filename, true);
+	saveFileReq.onreadystatechange = saveFileReqRes;
+	saveFileReq.send(contents);
+
+	function saveFileReqRes(){
+		if(this.readyState == 4){
+			if(this.status == 200){
+				cb('file saved');
+			} else {
+				cb('error saving file');
+			}
+		}
+	}
+}
